fb54c5747f71cddbc32fba8749c6ba78a1118e05
jamesycs <kaynejames@Kaynes-MacBook-Pro.local>
2017-07-31 13:48:17 +0800

    add node to portfilio

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+app.use(express.static("public"));
+const port = process.env.PORT || 3000;
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.listen(port, function() {
+  console.log('App listening on port 3000!');
+});
