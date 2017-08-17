3427493b74825e11a874e61ea38d104de665e1d9
jjcs <kaynejames@Kaynes-MacBook-Pro.local>
2017-08-17 19:40:54 +0800

    tuned footer

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
-var express = require('express');
-var app = express();
-var path = require('path');
+const express = require("express");
+const app = express();
+const path = require("path");
 app.use(express.static("public"));
 const port = process.env.PORT || 3000;
 
-
-console.log(path.join(__dirname + '/index.html'));
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
+console.log(path.join(__dirname + "/index.html"));
+app.get("/", function(req, res) {
+	res.sendFile(path.join(__dirname + "/index.html"));
 });
 
 app.listen(port, function() {
-  console.log('App listening on port 3000!');
+	console.log("App listening on port 3000!");
 });
